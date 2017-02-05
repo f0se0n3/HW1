7f4d21a0146c37bdfb3a6a8f961fb26a22941706
f0se0n3 <andrey.kosenko9@mail.ru>
2017-02-05 15:29:07 +0200

    hw1 done

--- src/NumAddition.java
+++ src/NumAddition.java
@@ -1,12 +1,66 @@
 public class NumAddition {
-    public static String addition(String input) {
+    public static int addition(String input) {
         char numberArray[] = input.toCharArray();
         int sum = 0;
-
         for(int i = 0; i < numberArray.length; i++) {
-            sum = sum+numberArray[i] - '0';
+            sum = sum + numberArray[i] - '0';
         }
-        System.out.println(sum);
-        return input;
+    System.out.println(sum);
+        if (sum > 9) {
+            addition2(sum);
+        } else {
+            resultOfCalculations(sum);
+        }
+    return sum;
+    }
+
+    public static int addition2(int sum) {
+        String sumStr = Integer.toString(sum);
+        char sumArray[] = sumStr.toCharArray();
+        int sum2 = 0;
+        for (int i = 0; i < sumArray.length; i++) {
+            sum2 = sum2 + sumArray[i] - '0';
+        }
+        System.out.println(sum2);
+        if (sum2 > 9) {
+            addition3(sum2);
+        } else {
+            resultOfCalculations(sum2);
+        }
+        return sum2;
+    }
+
+
+    public static int addition3(int sum2) {
+        String sumStr = Integer.toString(sum2);
+        char sumArray[] = sumStr.toCharArray();
+        int sum3 = 0;
+        for (int i = 0; i < sumArray.length; i++) {
+            sum3 = sum3 + sumArray[i] - '0';
+        }
+        resultOfCalculations(sum3);
+        return sum3;
+    }
+
+
+    public static int resultOfCalculations(int result) {
+        switch (result) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            default:
+                System.out.println(result);
+                break;
+        }
+        return result;
     }
 }
